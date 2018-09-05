9502126fbd0a18f8d4685aebcbe8b5388f0cbd59
robert <robert.piotrowski92@gmail.com>
2018-09-05 19:49:42 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Main {
+
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        Person person =  new Person(10,"TOm", "Jerry");
+
+
+        log.info("{} {}",person,"test");
+        
+
+
+
+    }
+}
