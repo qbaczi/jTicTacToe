e4854e4863ae61bd8aa03be2eb6e18450c9543ee
robert <robert.piotrowski92@gmail.com>
2018-09-26 17:38:24 +0200

    init clean project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,16 +7,6 @@ public class Main {
     private static Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-
-        Person person =  new Person(10,"TOm", "Jerry");
-
-
-        log.info("{} {}",person,"test");
-
-//utworzyc kolekcje stringow np. aaaaaaaa x 10000000
-
-        //zrobic heap dump'a
-        //pokazac mi go
 
 
     }
